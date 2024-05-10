1162a546265641dd2e7f2260909e6d12d5048632
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-05-11 00:13:47 +0530

    HTTP and packages installation

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const http = require('http')
+// npm - global command, comes with node
+// npm --version
 
-const server = http.createServer((req, res) => {
-    console.log(req)
-    if (req.url == "/") {
-      res.end("Welcome to our home page");
-    } else if (req.url == "/about") {
-      res.end("Welcome to our about page");
-    } else {
-        res.end(`
-            <h1>OOPS!!!!</h1>
-            <p>We can't seem to find the page you're loooking for!</p>
-            <a href='/'>Back Home</a>
-        `);
-    }
-})
+// local dependency - use it only in this particular project
+// npm i <packageName>
 
-server.listen(5000)
+// global dependency - use it in any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
+
+// package.json - manifest file (stores important infpo about project/package)
+// manual approach (create package.json in the root, create properties, etc.)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+// 2 ways for deleting a package:
+// 1 .npm uninstall <packageName>
+// 2. delete package-lock.json and node modules folder and then remove the package under dependencies in package.json() and then run npm i in the terminal
+
+const _ = require('lodash')
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
