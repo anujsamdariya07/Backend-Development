e56c1491bb896f018b9b7ed85bff1428c5356fa4
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-05-15 02:22:52 +0530

    Router setup and controllers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,18 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const people = require('./routes/people')
+const login = require('./routes/login')
+
+app.use(express.static('./methods-public'));
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use('/api/people', people)
+
+app.use('/login', login)
+
+app.listen(5000, (req, res) => {
+  console.log(`Server listening at port: 5000`);
+});
